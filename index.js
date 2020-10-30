ee809c97b813fc9c9426698b4713c5ddf38c3a65
spyder <31564156+ketan0896@users.noreply.github.com>
2020-10-30 18:14:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname+'/index.html');
@@ -35,6 +36,6 @@ io.on('connection', (socket) => {
 	})
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(port, () => {
+  console.log('listening on *:' + port);
 });
