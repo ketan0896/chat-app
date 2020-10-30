a20530f2a50eee0295998eeded5f5c5ade99ecb2
spyder <31564156+ketan0896@users.noreply.github.com>
2020-10-30 17:44:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', (socket) => {
+	io.emit('connections',Object.keys(io.sockets.connected).length);
+
+	socket.on('disconnect', () => {
+		io.emit('connections',Object.keys(io.sockets.connected).length);
+		//console.log('Disconnected');
+	})
+
+	socket.on('chat-message', (data)=>{
+		socket.broadcast.emit('chat-message', data);
+	});
+
+	socket.on('typing', (data)=>{
+		socket.broadcast.emit('typing', data);
+	});
+
+	socket.on('stopTyping', (data)=>{
+		socket.broadcast.emit('stopTyping', data);
+	});
+
+	socket.on('joined', (data) => {
+		socket.broadcast.emit('joined', data);
+	})
+
+	socket.on('leaved', (data) => {
+		socket.broadcast.emit('leaved', data);		
+	})
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
